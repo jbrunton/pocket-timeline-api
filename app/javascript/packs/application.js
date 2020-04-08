4ed1972ddafe678b006f1cc5ba402886a9d13cd7
John Brunton <jbrunton@zipcar.co.uk>
2020-04-08 21:37:04 +0100

    LRN-6 enable turbolinks, disable for sign in

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// see https://jbrunton.atlassian.net/browse/LRN-6
-// require("turbolinks").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
