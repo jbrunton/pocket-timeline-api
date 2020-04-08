3bd318614beed2c4163ca965704b8848be8a853a
John Brunton <jbrunton@zipcar.co.uk>
2020-04-08 21:57:43 +0100

    move google client:platform script to google.html.erb, add comment to provider_ignores_state flag

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, 'APP_ID', 'APP_SECRET'
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
   puts "configuring google_oauth2, #{ENV['GOOGLE_CLIENT_ID']}, #{ENV['GOOGLE_CLIENT_SECRET']}"
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], provider_ignores_state: Rails.env.development?
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+           # hacky fix to OmniAuth::Strategies::OAuth2::CallbackError (csrf_detected | CSRF detected)
+           # see https://github.com/omniauth/omniauth-oauth2/issues/58#issuecomment-43637421
+           provider_ignores_state: Rails.env.development?
 end
