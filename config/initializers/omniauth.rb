49d27c13d440dec66af6e708a209e4e72f841d8b
John Brunton <jbrunton@zipcar.co.uk>
2020-04-08 13:04:19 +0100

    add sessions controller

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
   #provider :facebook, 'APP_ID', 'APP_SECRET'
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  puts "configuring google_oauth2, #{ENV['GOOGLE_CLIENT_ID']}, #{ENV['GOOGLE_CLIENT_SECRET']}"
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+  post '/auth/:provider/callback', :to => 'sessions#create'
 end
