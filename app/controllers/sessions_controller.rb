b65ece2a5b42bde1150debf376b49c09e7d5aa5a
John Brunton <jbrunton@zipcar.co.uk>
2020-04-08 15:25:59 +0100

    create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
   def create
-    render plain: request.env['omniauth.auth'].inspect
+    auth_hash = request.env['omniauth.auth']
+    @auth = Authorization.find_from_hash(auth_hash)
+
+    # Create a new user or add an auth to existing user, depending on
+    # whether there is already a user signed in.
+    @auth = Authorization.create_from_hash(auth_hash, current_user) unless @auth
+
+    # Log the authorizing user in.
+    self.current_user = @auth.user
+
+    render plain: "Welcome, #{current_user.name}."
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :authorizations
+
+  def self.create_from_hash!(hash)
+    create(:name => hash['info']['name'])
+  end
 end
