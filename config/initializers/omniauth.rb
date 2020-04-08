da2fc1955cc2745b6cd61a2fe1ec94f48ce54bb3
John Brunton <jbrunton@zipcar.co.uk>
2020-04-08 20:44:22 +0100

    trying out one time code flow

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, 'APP_ID', 'APP_SECRET'
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
   puts "configuring google_oauth2, #{ENV['GOOGLE_CLIENT_ID']}, #{ENV['GOOGLE_CLIENT_SECRET']}"
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], provider_ignores_state: Rails.env.development?
 end
