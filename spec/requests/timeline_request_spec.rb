ea7101c5c2916d3e1f65d39d43e290a8a3daf8c6
John Brunton <jbrunton@zipcar.co.uk>
2020-04-11 19:42:00 +0100

    LRN-20 fix request tests

--- spec/requests/timeline_request_spec.rb
+++ spec/requests/timeline_request_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Timelines", type: :request do
 
-  describe "GET /index" do
+  describe "GET /timelines" do
     it "returns http success" do
-      get "/timeline/index"
+      get "/timelines"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/timeline/show"
+      timeline = Timeline.create(title: 'Test Timeline')
+      get "/timelines/#{timeline.id}"
       expect(response).to have_http_status(:success)
     end
   end
