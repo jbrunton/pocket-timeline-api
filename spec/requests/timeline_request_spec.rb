eb8a8f23829a80c9e98b822c5c5c62aebb29a827
John Brunton <jbrunton@zipcar.co.uk>
2020-04-14 17:27:53 +0100

    LRN-37 make json requests

--- spec/requests/timeline_request_spec.rb
+++ spec/requests/timeline_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Timelines", type: :request do
 
   describe "GET /timelines" do
     it "returns http success" do
-      get "/timelines"
+      get "/timelines.json"
       expect(response).to have_http_status(:success)
     end
   end
@@ -12,7 +12,7 @@ RSpec.describe "Timelines", type: :request do
   describe "GET /show" do
     it "returns http success" do
       timeline = Timeline.create(title: 'Test Timeline')
-      get "/timelines/#{timeline.id}"
+      get "/timelines/#{timeline.id}.json"
       expect(response).to have_http_status(:success)
     end
   end
