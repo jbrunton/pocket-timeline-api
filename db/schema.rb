edab3684745ebcf787d7890c76ea8ad2fb81a1a0
John Brunton <jbrunton@zipcar.co.uk>
2020-04-15 20:31:06 +0100

    LRN-45 add categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_104631) do
+ActiveRecord::Schema.define(version: 2020_04_15_193045) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string "provider"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_04_14_104631) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_authorizations_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "timeline_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["timeline_id"], name: "index_categories_on_timeline_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -58,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_104631) do
   end
 
   add_foreign_key "authorizations", "users"
+  add_foreign_key "categories", "timelines"
   add_foreign_key "events", "timelines"
   add_foreign_key "ratings", "timelines"
   add_foreign_key "ratings", "users"
