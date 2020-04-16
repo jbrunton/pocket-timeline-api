d4e41ea544a34812f7ed3118b218c54136b70c65
John Brunton <jbrunton@zipcar.co.uk>
2020-04-16 11:10:42 +0100

    LRN-45 fix test

--- spec/requests/categories_request_spec.rb
+++ spec/requests/categories_request_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe "Categories", type: :request do
 
   describe "GET /show" do
     it "returns http success" do
-      get "/categories/show"
+      timeline = Timeline.create(title: 'Test Timeline')
+      category = Category.create(name: 'Test Category', timeline: timeline)
+
+      get "/categories/#{category.id}"
+
       expect(response).to have_http_status(:success)
     end
   end
